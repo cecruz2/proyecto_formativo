24487c94b322f89b76375e14d4d76050b6c29a85
cecruz2 <cecruz20@misena.edu.co>
2019-08-06 18:12:22 -0500

    Initial commit

---
+++ proyecto_formativo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace proyecto_formativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            proyecto categoria = new proyecto();
+            proyecto categoria1 = new proyecto("elite", 18, 21);
+
+
+            //categoria.IngresaDatos();
+            //categoria.ImprimeDatos();
+            categoria1.Modificar();
+            categoria1.ImprimeDatos();
+            Console.ReadKey();
+        
+        }
+    }
+}
+//integrantes del proyecto formatovo:
+//Carlos eduardo cruz aguilar
+//Miguel Alejandro Luis Lopez
---
+++ proyecto_formativo/proyecto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace proyecto_formativo
+{
+    class proyecto
+    {
+        #region Propiedades
+        public string Nombre { get; set; }
+        public int EdadMin { get; set; }
+        public int EdadMax { get; set; }
+
+        #endregion
+        #region metodos 
+
+        public void IngresaDatos()
+        {
+            Console.WriteLine("ingrese el nombre de la categoria: ");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("digite la edad minima: ");
+            EdadMin = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la eada maxima de la categoria: ");
+            EdadMax = int.Parse(Console.ReadLine());
+        }
+        public void ImprimeDatos()
+        {
+            Console.WriteLine($"nombre: {Nombre}");
+            Console.WriteLine($"Edad minima: {EdadMin}");
+            Console.WriteLine($"Edad maxima: {EdadMax}");
+        }
+        public void Modificar()
+        {
+            Console.WriteLine("digite el rango de edad a modificar");
+            EdadMax = int.Parse(Console.ReadLine());
+        }
+
+        #endregion
+        #region constructores
+        public proyecto(string nombre, int edadmin, int edadmax)
+        {
+            Nombre = nombre;
+            EdadMin = edadmin;
+            EdadMax = edadmax;
+        }
+        public proyecto()
+        {
+
+        }
+        #endregion
+
+    }
+}
